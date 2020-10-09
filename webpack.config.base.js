b8131eda8975a4b617833627a2ce686455af997e
ike <2740262535@qq.com>
2020-10-09 10:42:57 +0800

    提交

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,14 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'index.[contenthash].js'
+        filename: "index.[contenthash].js"
     },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'XDML - 写代码啦',
-        template: 'src/assets/index.html'
-    })
-    ]
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "XDML - 写代码啦",
+            template: "src/assets/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.styl$/,
+                loader: ["style-loader", "css-loader", "stylus-loader"]
+            },
+            {
+                test: /\.less$/,
+                loader: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("dart-sass")
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,34 +1,34 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const base = require('./webpack.config.base.js');
+const path = require("path");
+const base = require('./webpack.config.base.js')
 
 module.exports = {
     ...base,
-    mode: 'production',
+    mode: "production",
     plugins: [
         ...base.plugins,
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
             chunkFilename: '[id].[contenthash].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
         }),
-
     ],
     module: {
         rules: [
+            ...base.module.rules,
             {
                 test: /\.css$/i,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: '/public/path/to/',
+                            publicPath: '../',
                         },
                     },
                     'css-loader',
                 ],
-            },
-        ],
-    },
-
+            }
+        ]
+    }
 };
