cc93c366d76de13de484f79343bc5706c21f59f3
ike <2740262535@qq.com>
2020-10-11 20:12:12 +0800

    xx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import x from './x.js'
 
-console.log(x)
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy')
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log('模块加载错误')
+    })
+}
+
+div.appendChild(button)
